db11dca98be37042402a6f2c1c62fce52cda499f
Assiya Nurdaulet <assiyanurdaulet@MacBook-Air--Assiya.local>
2024-07-17 12:19:53 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const slideInterval = 3000;
+ 
+function changeSlide() {
+  slides[currentIndex].style.opacity = 0;
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].style.opacity = 1;
+}
+ 
+setInterval(changeSlide, slideInterval);
